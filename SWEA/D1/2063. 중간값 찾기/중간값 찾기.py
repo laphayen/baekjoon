360832a1e82c51b1995f7d67516f416f272befa3
Baek Gi Chan <laphayen@gmail.com>
2024-06-26 23:40:00 +0900

    [D1] Title: 중간값 찾기, Time: 101 ms, Memory: 42,384 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
@@ -1,6 +1,19 @@
-N = int(input())
-
-l = list(map(int, input().split()))
-l.sort()
-mid = int(N / 2)
-print(l[mid])
+import java.util.*;
+import java.io.FileInputStream;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {       
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i=0; i<n; ++i){
+            arr[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(arr);
+        System.out.printf("%d", arr[n/2]);
+    }
+}
