98abff52fd912bd2315dbad07fdfd8d4b5b5bf2a
Baek Gi Chan <laphayen@gmail.com>
2024-07-10 11:39:06 +0900

    [Bronze III] Title: 곱셈, Time: 168 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // 입력 받기
-        int a = scanner.nextInt();
-        String b = scanner.next();
-
-        // b의 각 자리 숫자에 대해 거꾸로 곱셈 수행 및 출력
-        for (int i = b.length() - 1; i >= 0; i--) {
-            int digit = Character.getNumericValue(b.charAt(i));
-            System.out.println(a * digit);
-        }
-
-        // a와 b 전체 숫자 곱셈 결과 출력
-        System.out.println(a * Integer.parseInt(b));
-
-        scanner.close();
-    }
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		String num2 = sc.next();
+		
+		for (int i = num2.length() - 1; i >= 0; i--) {
+			int digit = Character.getNumericValue(num2.charAt(i));
+			System.out.println(num1 * digit);
+		}
+		
+		
+		System.out.println(num1 * Integer.parseInt(num2));
+	}
 }
