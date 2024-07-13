fc370136bb25915e008e886590ffa45823d0211b
Baek Gi Chan <laphayen@gmail.com>
2024-07-13 21:49:54 +0900

    [D1] Title: 스탬프 찍기, Time: 136 ms, Memory: 20,516 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Solution {
+	
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		String s = "#";
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.print(s);
+		}
+		
+	}
+
+}
