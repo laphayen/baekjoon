b8c8c1c18b766bb33dff448dc207a053f6daae15
Baek Gi Chan <laphayen@gmail.com>
2024-07-14 03:41:54 +0900

    [D1] Title: 더블더블, Time: 147 ms, Memory: 20,336 KB -BaekjoonHub

---
+++ SWEA/D1/2019. 더블더블/더블더블.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Solution {
+	
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int value = 1;
+		for (int i=0; i<=n; i++) {
+			System.out.print(value + " ");
+			value *= 2;
+		}
+	}
+
+}
