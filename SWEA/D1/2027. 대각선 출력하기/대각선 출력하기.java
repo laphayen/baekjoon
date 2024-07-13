75aa98d1ba44c760cce4527d70b32f748002b104
Baek Gi Chan <laphayen@gmail.com>
2024-07-14 03:26:21 +0900

    [D1] Title: 대각선 출력하기, Time: 126 ms, Memory: 19,736 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Solution {
+	
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (i == j) {
+					System.out.print("#");
+				}
+				else {
+					System.out.print("+");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
