57edee0c7d3660d483ac59278b46026d82bff762
Baek Gi Chan <laphayen@gmail.com>
2024-07-13 21:34:15 +0900

    [D1] Title: 신문 헤드라인, Time: 161 ms, Memory: 19,948 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Solution {
+	
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		
+		String up = s.toUpperCase();
+		
+		System.out.println(up);
+		
+	}
+
+}
