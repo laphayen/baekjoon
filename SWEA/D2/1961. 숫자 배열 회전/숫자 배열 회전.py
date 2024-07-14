b6b632f0be74084dffd5fc6c4658619c898dc413
Baek Gi Chan <laphayen@gmail.com>
2024-07-14 22:21:37 +0900

    [D2] Title: 숫자 배열 회전, Time: 97 ms, Memory: 43,792 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,16 +1,47 @@
-T = int(input())
-for tc in range(1, T+1):
-    N = int(input())
-    l = [list(map(int, input().split())) for _ in range(N)]
-    l90 = [[0 for _ in range(N)] for _ in range(N)]
-    l180 = [[0 for _ in range(N)] for _ in range(N)]
-    l270 = [[0 for _ in range(N)] for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            l90[i][j] = l[N-1-j][i]
-            l180[i][j] = l[N-1-i][N-1-j]
-            l270[i][j] = l[j][N-1-i]
-    print('#%d' %(tc))
-    for i in range(N):
-        print(''.join(list(map(str, l90[i]))), ''.join(list(map(str, l180[i]))),
-              ''.join(list(map(str, l270[i]))))
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Solution {
+	
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int test = sc.nextInt();
+		for(int t = 1; t <= test; t++) {
+			int n = sc.nextInt();
+			
+			int[][] a = new int[n][n];
+			
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					a[i][j] = sc.nextInt();
+				}
+			}
+			
+			System.out.println("#" + t + " ");
+			
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					System.out.print(a[n-j-1][i]);
+				}
+				
+				System.out.print(" ");
+				
+				for(int j = 0; j < n; j++) {
+					System.out.print(a[n-i-1][n-j-1]);
+				}
+				
+				System.out.print(" ");
+				
+				for(int j = 0; j < n; j++) {
+					System.out.print(a[j][n-1-i]);
+				}
+				System.out.println();
+				
+			}
+		}
+	}
+
+}
