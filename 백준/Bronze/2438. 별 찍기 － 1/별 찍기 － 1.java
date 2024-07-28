b473f446f8f4d9aeb523aec227643ed50b80518b
Baek Gi Chan <laphayen@gmail.com>
2024-07-28 15:43:08 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 312 ms, Memory: 18196 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		char star = '*';
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+}
